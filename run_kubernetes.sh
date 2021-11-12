7899df8ee86ae244d088fa689011a02226228fe5
ElsieMarion <elcymao@gmail.com>
2021-11-12 03:50:56 -0800

    Update run_kubernetes.sh
    exposing port 80 to allow connection to kubectl

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,7 +1,7 @@
 dockerpath=elsiedev/elsie-devk
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run elsie --image=$dockerpath --port=80
+kubectl run demolocal --image=$dockerpath --port=80
 # Step 3:
 # List kubernetes pods
 kubectl get pods 
